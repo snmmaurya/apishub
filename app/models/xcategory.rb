cff7b65e075fe9fd1aabf0ffe9e7c54d9f1ff373
Snm Maurya <snmmaurya@gmail.com>
2018-05-09 12:09:48 +0530

    fix

--- app/models/xcategory.rb
+++ app/models/xcategory.rb
@@ -5,7 +5,7 @@ class Xcategory < ApplicationRecord
   # has_many :xpositions, through: :xcategory_xpositions
   has_many :xpositions
 
-  validates :title, :description, :xcategory, :status, :image, presence: true
+  validates :name, :description, :xcategory, :status, :image, presence: true
 
   scope :active, -> {where(status: :on)}
 
